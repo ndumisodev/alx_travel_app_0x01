e06edc6be5ac20bd19a5bf8e2d41604ab5c94498
nngobese023 <nngobese023@student.wethinkcode.co.za>
2025-08-09 15:31:54 +0200

    django project

--- alx_travel_app/listings/urls.py
+++ alx_travel_app/listings/urls.py
@@ -1,17 +1,11 @@
-from rest_framework import permissions
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
+from rest_framework.routers import DefaultRouter
+from django.urls import path, include
+from .views import ListingViewSet, BookingViewSet
 
-schema_view = get_schema_view(
-   openapi.Info(
-      title="Travel API",
-      default_version='v1',
-      description="API for Listings and Bookings",
-   ),
-   public=True,
-   permission_classes=(permissions.AllowAny,),
-)
+router = DefaultRouter()
+router.register(r'listings', ListingViewSet)
+router.register(r'bookings', BookingViewSet)
 
-urlpatterns += [
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+urlpatterns = [
+    path('api/', include(router.urls)),
 ]
